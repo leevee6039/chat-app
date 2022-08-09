0979bd87a6ffb8d19cc42ceb0b6fefe4a29ec823
leevee6039 <leevee6039@gmail.com>
2022-08-09 00:05:40 -0400

    added CustomActions.js for communication features

--- components/Chat.js
+++ components/Chat.js
@@ -3,6 +3,8 @@ import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { Bubble, GiftedChat, InputToolbar } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
+import CustomActions from './CustomActions';
+import MapView from 'react-native-maps';
 
 const firebase = require('firebase');
 require('firebase/firestore');
@@ -16,6 +18,7 @@ export default class Chat extends React.Component {
       user: {
         _id: '',
         name: '',
+        avatar: '',
       },
       isConnected: null,
     };
@@ -47,12 +50,15 @@ export default class Chat extends React.Component {
       let data = doc.data();
       messages.push({
         _id: data._id,
-        text: data.text,
+        text: data.text || '',
         createdAt: data.createdAt.toDate(),
         user: {
           _id: data.user._id,
           name: data.user.name,
+          avatar: data.user.avatar || '',
         },
+        image: data.image || null,
+        location: data.location || null,
       });
     });
     this.setState({
@@ -176,7 +182,7 @@ export default class Chat extends React.Component {
     this.referenceChatMessages.add({
       uid: this.state.uid,
       _id: message._id,
-      text: message.text,
+      text: message.text || '',
       createdAt: message.createdAt,
       user: message.user,
     });
@@ -207,8 +213,32 @@ export default class Chat extends React.Component {
     }
   }
 
+  // actions '+'
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+
+  // for the map
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+
   render() {
-    let { color, name } = this.props.route.params;
+    let color = this.props.route.params.color;
 
     return (
       <>
@@ -216,9 +246,11 @@ export default class Chat extends React.Component {
           <GiftedChat
             renderBubble={this.renderBubble.bind(this)}
             renderInputToolbar={this.renderInputToolbar.bind(this)}
+            renderActions={this.renderCustomActions}
+            renderCustomView={this.renderCustomView}
             messages={this.state.messages}
             onSend={(messages) => this.onSend(messages)}
-            user={{ _id: this.state.user._id, name: name }}
+            user={{ _id: this.state.user._id, name: this.state.user.name }}
           />
           {/* Avoid keyboard to overlap text messages on older Andriod versions */}
           {Platform.OS === 'android' ? (
